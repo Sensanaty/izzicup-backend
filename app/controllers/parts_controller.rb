d33edec5aa6fbcc421ff95330d437de7e498f26a
Luka Salevic <salevic@lu-ka.me>
2020-06-12 02:05:50 +0200

    Implement routes for Parts requests & fix a typo in Auth

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,2 +1,44 @@
 class PartsController < ApplicationController
+  before_action :set_part, only: [:show, :update, :destroy]
+
+  def index
+    @parts = Part.all
+
+    render json: @parts
+  end
+
+  def show
+    render json: @part
+  end
+
+  def create
+    @part = Part.new(part_params)
+
+    if @part.save
+      render json: @part, status: :created, location: @part
+    else
+      render json: @part.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @part.update(part_params)
+      render json: @part
+    else
+      render json: @part.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @part.destroy
+  end
+
+  private
+  def set_part
+    @part = Part.find(params[:id])
+  end
+
+  def part_params
+    params.require(:part).permit!
+  end
 end
