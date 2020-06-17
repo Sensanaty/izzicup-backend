cde4c9a60d5dc290de345c3c16be090d28ab5584
Luka Salevic <salevic@lu-ka.me>
2020-06-17 04:00:30 +0200

    Add User model specs
    Make sure passwords cannot be nil or empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, allow_blank: false
   validates :password,
-            presence: true,
             format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}\Z/, message: "Password must be at least 8 characters long, contain at least 1 uppercase and 1 lowercase letter and 1 number" },
-            confirmation: true,
-            on: :create
+            confirmation: true
 end
