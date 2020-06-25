05e796fbdc4e222e3841f2217b99983449174fb9
Luka Salevic <salevic@lu-ka.me>
2020-06-25 18:09:36 +0200

    Seed 500 items instead of 50 and fix cost_min errors
    Because of the >0 validation on cost_min and the way I used the floor method, sometimes the random number would (apparently) come to 0 or less

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ puts "Email: test@email.com\nPassword: Password1"
 User.create!(email: "admin@email.com", password: "Password1", password_confirmation: "Password1", admin: true)
 puts "Email: admin@email.com\nPassword: Password1"
 
-puts "Generating 50 parts"
-50.times do
-  Part.create(part_number: Faker::Alphanumeric.alphanumeric(number: rand(9..16)).upcase,
+puts "Generating 500 parts"
+500.times do
+  new = Part.create(part_number: Faker::Alphanumeric.alphanumeric(number: rand(9..16)).upcase,
               owner: Faker::Company.name,
               date_added: DateTime.now,
               description: Faker::ElectricalComponents.passive + " " + Faker::Appliance.equipment,
@@ -17,11 +17,14 @@ puts "Generating 50 parts"
               reserved: rand(1..10),
               sold: rand(0..50),
               condition: "NE",
-              cost_min: rand(200..3000).floor(-3),
+              cost_min: 3000,
               price_min: 3500,
               order_min: 10,
               lead_time: "3 DAYS",
               quote_type: "OUTRIGHT SALE",
               tag: "MOQ 1123123 JABALABADADAD"
               )
+  if new.errors
+    puts new.errors.full_messages
+  end
 end
